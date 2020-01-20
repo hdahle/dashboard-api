de7efb1697f5d947f5f70617dabebaec3088ed33
Håkon Dahle <hakon.dahle@gmail.com>
2020-01-20 05:04:46 +0100

    cleaned up the maunaloa scripts, source and link keys

--- connectors/co2-maunaloa/ch4-maunaloa.sh
+++ connectors/co2-maunaloa/ch4-maunaloa.sh
@@ -6,21 +6,16 @@
 REDISKEY="maunaloach4"
 TMPDIR=$(mktemp -d)
 
-
 echo "Getting data from noaa.gov, writing to: ${TMPDIR}/${REDISKEY}.txt"
-
 curl ftp://aftp.cmdl.noaa.gov/products/trends/ch4/ch4_mm_gl.txt > ${TMPDIR}/${REDISKEY}.txt
 
 echo "Converting data to JSON, writing to: ${TMPDIR}/${REDISKEY}.json"
-
 awk 'BEGIN {ORS="";
             print "{"
-            print "\"source\":\"https://www.esrl.noaa.gov/gmd/ccgg/trends_ch4/\", "
-            print "\"ftp\":\"ftp://aftp.cmdl.noaa.gov/products/trends/ch4/ch4_mm_gl.txt\", "
-            print "\"attribution\":"
-            print "\"Ed Dlugokencky, NOAA/ESRL (www.esrl.noaa.gov/gmd/ccgg/trends_ch4/)\", "
-            print "\"info\":"
-            print "\" \", "
+            print "\"source\":"
+            print "\"Ed Dlugokencky, NOAA/ESRL, http://www.esrl.noaa.gov/gmd/ccgg/trends_ch4\", "
+            print "\"link\":\"ftp://aftp.cmdl.noaa.gov/products/trends/ch4/ch4_mm_gl.txt\", "
+            print "\"info\":\"Globally averaged monthly mean CH4 abundance measured from marine surface sites. Numbers are dry air mole fraction in ppb.\", "
             print "\"data\": ["
             NOTFIRST=0
            }
@@ -36,6 +31,7 @@ cat ${TMPDIR}/${REDISKEY}.json | wc --bytes
 # Stick it into Redis
 echo "Saving to Redis "
 redis-cli -x set ${REDISKEY} < ${TMPDIR}/${REDISKEY}.json
+
 # Quick verification
 echo "Retrieving from Redis, JSON byte count:"
 redis-cli get ${REDISKEY} | wc --bytes
--- connectors/co2-maunaloa/co2-maunaloa.sh
+++ connectors/co2-maunaloa/co2-maunaloa.sh
@@ -5,24 +5,20 @@
 #
 # H. Dahle
 
-REDISKEY=maunaloaco2
+REDISKEY="maunaloaco2"
 TMPDIR=$(mktemp -d)
 
-echo "Fetch monthly stats from Mauna Loa, saving to ${REDISKEY}.txt"
-echo "Using tempdir ${TMPDIR}"
-
+echo "Getting ftp data from noaa.gov, saving to ${TMPDIR}/${REDISKEY}.txt"
 curl ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_mm_mlo.txt > ${TMPDIR}/${REDISKEY}.txt
 
 echo "Converting data to JSON, saving to ${TMPDIR}/${REDISKEY}.json"
-
 awk 'BEGIN {ORS="";
             print "{"
-            print "\"source\":\"www.esrl.noaa.gov/gmd/ccgg/trends/\", "
-            print "\"ftp\":\"ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_mm_mlo.txt\", "
-            print "\"attribution\":"
+            print "\"source\":"
             print "\"Dr. Pieter Tans, NOAA/ESRL (www.esrl.noaa.gov/gmd/ccgg/trends/) and "
             print "Dr. Ralph Keeling, Scripps Institution of Oceanography (scrippsco2.ucsd.edu/)\", "
-            print "\"historical\":"
+            print "\"link\":\"ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_mm_mlo.txt\", "
+            print "\"info\":"
             print "\"Before the Industrial Revolution in the 19th century, global average CO2 was about 280 ppm. "
             print "During the last 800,000 years, CO2 fluctuated between about 180 ppm during ice ages and "
             print "280 ppm during interglacial warm periods.\", "
@@ -35,11 +31,14 @@ awk 'BEGIN {ORS="";
             printf "{\"date\":\"%s-%02d\",\"average\":%s,\"interpolated\":%s,\"trend\":%s}",  $1, $2, $4, $5, $6 }
      END   {print "]}"}' < ${TMPDIR}/${REDISKEY}.txt > ${TMPDIR}/${REDISKEY}.json
 
+# Just for reassurance
 echo "JSON byte count:"
 cat ${TMPDIR}/${REDISKEY}.json | wc --bytes
 
+# Save to redis
 echo "Saving JSON to Redis, key: ${REDISKEY}"
 redis-cli -x set ${REDISKEY} < ${TMPDIR}/${REDISKEY}.json
 
+# Quick verification
 echo "Retrieving from Redis, JSON byte count:"
 redis-cli get ${REDISKEY} | wc --bytes
