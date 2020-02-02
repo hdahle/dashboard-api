30cfac6cb034fe6f21e3426a6f25cf634e44b6f3
Håkon Dahle <hakon.dahle@gmail.com>
2020-02-02 20:36:05 +0100

    new connector: hadcrut

---
+++ connectors/hadcrut/hadcrut-temperature.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+# Fetch temperature data from UK Met Office / Hadley
+# Convert to JSON and store to Redis
+#
+# H. Dahle
+
+REDISKEY="global-temperature-hadcrut"
+TMPDIR=$(mktemp -d)
+CSVFILE="${TMPDIR}/${REDISKEY}.csv"
+JSONFILE="${TMPDIR}/${REDISKEY}.json"
+DATE=`date --iso-8601='minutes'`
+
+echo ${DATE}
+echo "Getting ftp data from crudata.uea.ac.uk, saving to ${CSVFILE}"
+curl "https://crudata.uea.ac.uk/cru/data/temperature/HadCRUT4-gl.dat" > ${CSVFILE}
+ 
+if [ -f "${CSVFILE}" ]; then
+    echo -n "Number of lines in CSV:"
+    cat ${CSVFILE} | wc -l
+else
+    echo "File not found: ${CSVFILE}, aborting "
+    exit 0
+fi
+
+# CSV format: year + 12 months + annual:
+# We skip every other line, the lines which have 13 columns
+#
+# 1851 -0.296 -0.356 -0.479 -0.441 -0.295 -0.197 -0.212 -0.157 -0.101 -0.057 -0.020 -0.051 -0.218
+# 1851     23     22     20     21     20     21     22     23     19     20     18     20
+# 1852 -0.315 -0.477 -0.502 -0.557 -0.211 -0.040 -0.018 -0.202 -0.125 -0.216 -0.193  0.073 -0.228
+# 1852     23     22     22     23     23     24     23     24     21     22     22     25
+# 1853 -0.182 -0.327 -0.309 -0.355 -0.268 -0.175 -0.059 -0.148 -0.404 -0.362 -0.255 -0.437 -0.269
+ 
+echo "Converting data to JSON, saving to ${JSONFILE}"
+awk -v d="${DATE}" 'BEGIN {
+            ORS="";FS=" ";
+            print "{"
+            print "\"source\":\"HadCRUT4 Dataset, Climatic Research Unit, University of East Anglia, UK\", "
+            print "\"license\":\"These datasets are made available under the Open Database License. "
+            print "Any rights in individual contents of the datasets are licensed under the "
+            print "Database Contents License under the conditions of Attribution and Share-Alike. "
+            print "Please use the attribution Climatic Research Unit, University of East Anglia\", "
+            print "\"link\":\"https://crudata.uea.ac.uk/cru/data/temperature/HadCRUT4-gl.dat\", "
+            print "\"accessed\":\"" d "\", "
+            print "\"legend\":\"The temperature data is organized in year/value pairs: [ {y:year, x:annualMean}, ... ] \", "
+            print "\"data\": ["
+            NOTFIRST=0
+           }
+     /^#/  {next}
+     NF == 14 {
+            if (NOTFIRST) print ", "
+            NOTFIRST=1
+            print "{\"x\":" $1 ",\"y\":" $14 " }"
+          }
+     END  { print "]}" }' < ${CSVFILE} > ${JSONFILE}
+
+# Just for reassurance
+echo -n "JSON byte count:"
+cat ${JSONFILE} | wc --bytes
+
+# When installing cron job, provde fully qualified filename to this script
+REDIS=$1
+if [ "$REDIS" = "" ]; then
+  REDIS="redis-cli"
+else
+  if [ ! -f ${REDIS} ]; then
+    echo "Redis-executable not found: ${REDIS}, not storing in Redis"
+    exit
+  fi
+fi
+
+# Save to redis
+echo -n "Saving JSON to Redis, key: ${REDISKEY}: "
+${REDIS} -x set ${REDISKEY} < ${JSONFILE}
+
+# Quick verification
+echo -n "Retrieving from Redis, JSON byte count: "
+${REDIS} get ${REDISKEY} | wc --bytes
---
+++ connectors/hadcrut/install.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+#
+# Installer for "hadcrut-temperature.sh"
+# Add script to crontab if it doesn't already exist
+# Run this scripts twice a month. Data is updated by NOAA monthly, time of month uncertain
+#
+# H. Dahle
+
+PWD=`pwd`
+
+# must use eval for tilde-expansion to work...dirty
+LOGDIR=`eval echo ~${USER}/log`
+SCRIPT="hadcrut-temperature.sh"
+LOGFILE="${LOGDIR}/cron.log"
+REDIS=`which redis-cli`
+echo "Using Redis at ${REDIS}"
+echo "Logs are in ${LOGDIR}"
+
+# check if log directory exists
+if [ ! -d "${LOGDIR}" ]; then
+    echo "Creating ${LOGDIR}"
+    mkdir ${LOGDIR} 
+    if [ ! -d "${LOGDIR}" ]; then
+      echo "Could not create ${LOGDIR} - aborting"
+      exit
+    else
+      echo "Logdir created"
+    fi
+else
+  echo "Using logfile: ${LOGFILE}"
+fi
+
+# make sure script exists
+if [ -f "${PWD}/${SCRIPT}" ]; then
+  echo "Shell script found"
+else
+  echo "Not found: ${PWD}/${SCRIPT} - aborting"
+  exit
+fi
+
+# make new crontab entry, twice a month, on 10th and 20th
+NEWENTRY="0 0 10,20 * * ${PWD}/${SCRIPT} ${REDIS} >> ${LOGFILE} 2>&1"
+echo "Creating crontab entry: ${NEWENTRY}"
+
+# test if new entry already exists
+crontab -l > newcrontab
+EXISTENTRY=`grep -F "${NEWENTRY}" < newcrontab`
+
+# add to crontab
+if [ "${EXISTENTRY}" = "${NEWENTRY}"  ]; then
+  echo "Already in crontab"
+else
+  echo "Adding new entry to crontab"
+  echo "${NEWENTRY}" >> newcrontab
+  crontab newcrontab
+fi
+
+# clean up
+if [ -f newcrontab ]; then
+  echo "Cleaning up"
+  rm newcrontab
+fi
+
