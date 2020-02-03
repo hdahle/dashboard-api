cc8e2345a1b569fdadf40b378cd04ac0d3748cbe
Håkon Dahle <hakon.dahle@gmail.com>
2020-02-03 16:04:02 +0100

    new csiro-recons data and json-structure

--- connectors/sealevel/csiro-recons.sh
+++ connectors/sealevel/csiro-recons.sh
@@ -1,9 +1,33 @@
 #!/bin/sh
 
 # Global Mean Sea Levels since 1880
-# The data files are available at https://research.csiro.au/slrwavescoast/?ddownload=327
-# This is a big ZIP archive, with the relevant CSV data file somewhere inside
-# Manually save the relevant CSV file, and then run this script to create JSON data and store in Redis
+# The CSV file is available at ftp://ftp.csiro.au/legresy/gmsl_files/CSIRO_Recons_gmsl_yr_2015.csv
+#
+# H. Dahle 2020
+
+
+REDISKEY="CSIRO_Recons_2015"
+TMPDIR=$(mktemp -d)
+CSVFILE="${REDISKEY}.csv"
+JSONFILE="${TMPDIR}/${REDISKEY}.json"
+DATE=`date --iso-8601='minutes'`
+
+echo ${DATE}
+echo "Global Sea Level. Converting from CSV to JSON"
+echo "Using temporary directory ${TMPDIR}"
+
+if [ ! -f "${CSVFILE}" ]; then
+  echo -n "File not found: ${CSVFILE}, downloading: "
+  curl -s -S "ftp://ftp.csiro.au/legresy/gmsl_files/CSIRO_Recons_gmsl_yr_2015.csv" > ${CSVFILE}
+fi
+
+if [ -f "${CSVFILE}" ]; then
+  echo -n "Number of lines in CSV, ${CSVFILE}: "
+  cat ${CSVFILE} | wc -l
+else
+  echo "No ${CSVFILE} - aborting"
+  exit
+fi
 
 # Input CSV Format
 #==> CSIRO_Recons.csv <==
@@ -14,55 +38,57 @@
 
 # JSON Output Format
 
-REDISKEY="CSIRO_Recons"
-TMPDIR=$(mktemp -d)
-
-echo "Global Sea Level. Converting from CSV to JSON"
-echo "Download and extract CSV from ZIP  at https://research.csiro.au/slrwavescoast/sea-level/measurements-and-data/sea-level-data/"
-echo "Using file ${REDISKEY}.csv"
-echo "Using temporary directory ${TMPDIR}"
-
-# Make sure file exists before we start
-if [ -f "${REDISKEY}.csv" ]; then
-    echo "Found input file"
-else
-    echo "File ${REDISKEY}.csv not found. Abort"
-    exit
-fi
-
 # Convert from CSV to JSON
-awk 'BEGIN {ORS="";
+awk -v d="${DATE}" 'BEGIN {
+            ORS="";
             FS=",";
             print "{"
             print "\"source\": \"Commonwealth Scientific and Industrial Research Organisation (CSIRO), Australia.Church, John; White, Neil (2016): Reconstructed Global Mean Sea Level for 1870 to 2001. v2. \", "
             print "\"link\": \"https://research.csiro.au/slrwavescoast/sea-level/measurements-and-data/sea-level-data/\", "
             print "\"attribution\": "
-            print "\"Church, John; White, Neil (2016): Reconstructed Global Mean Sea Level for 1870 to 2001. v2. CSIRO. Data Collection. https://doi.org/10.4225/08/57BF859E3ACE4\", "
+            print "\"Church, J.A. and N.J. White (2011), Sea-level rise from the late 19th to the early 21st century. Surveys in Geophysics, 32, 585-602, doi:10.1007/s10712-011-9119-1. This paper is published _Open Access_\", "
             print "\"info\": \"This file contains the monthly Global Mean Sea Level (GMSL) time series as shown on figure 2 of Church and White (2006). The sea level was reconstructed as described in Church et al (2004). \", "
             print "\"license\": "
             print "\"Creative Commons Attribution 4.0 International Licence\", "
-            print "\"data\": [{\"region\":\"global\", \"data\":["
+            print "\"accessed\": \"" d "\", "
+            print "\"data\":["
             FIRST=1
      }
-
-     NF!=3 {next}
+     
+     NF!=3  {next}
+     
      /Time/ {next}
 
-     {
+     {      gsub(/ /, "", $0)
             if (!FIRST) print ", "
             FIRST=0
-            printf "{\"year\":\"%d\",\"data\":%.1f,\"uncertainty\":%.1f}", $1, $2, $3
+            print "{"
+            print "\"t\":\"" substr($1,1,4) "-06-30\"," 
+            print "\"data\":" $2 ", " 
+            print "\"uncertainty\":"  $3 
+            print "}"
      }
-     END   {print "]}]}"}' < ${REDISKEY}.csv  > ${TMPDIR}/${REDISKEY}.json
+     END   {print "]}"}' < ${CSVFILE} > ${JSONFILE}
 
+# Quick test
+echo -n "JSON byte count:"
+cat ${JSONFILE} | wc --bytes
 
-# Just some reassurance
-echo "Saving JSON data, bytes"
-cat ${TMPDIR}/${REDISKEY}.json | wc --bytes
+# When installing cron job, provde fully qualified filename to this script
+REDIS=$1
+if [ "$REDIS" = "" ]; then
+  REDIS="redis-cli"
+else
+  if [ ! -f ${REDIS} ]; then
+    echo "Redis-executable not found: ${REDIS}, not storing in Redis"
+    exit
+  fi
+fi
 
-# stick it into Redis
-redis-cli -x set ${REDISKEY} < ${TMPDIR}/${REDISKEY}.json
+# Save to redis
+echo -n "Saving JSON to Redis, key: ${REDISKEY}: "
+${REDIS} -x set ${REDISKEY} < ${JSONFILE}
 
 # Quick verification
-echo "Retrieving from Redis, number of bytes:"
-redis-cli get ${REDISKEY} | wc --bytes
+echo -n "Retrieving from Redis, JSON byte count: "
+${REDIS} get ${REDISKEY} | wc --bytes
