355fcbcbc20a0ae8af0d7046e1579d6364fcfcf1
Håkon Dahle <hakon.dahle@gmail.com>
2020-02-09 12:03:53 +0100

    new EIA connector global coal/oil/gas

--- connectors/eia/eia.js
+++ connectors/eia/eia.js
@@ -1,8 +1,8 @@
 // eia.js
 //
-// Fetches coal production data from api.eia.gov
+// Fetches coal/oil/gas production data from api.eia.gov
 // Fetches from all 8 global regions
-// Reult from EIA is JSON, but we convert to more useful format
+// Reult from EIA is JSON, but we convert to chart.js-friendly format
 //
 // H. Dahle, 2020
 
@@ -10,8 +10,8 @@ var fetch = require('node-fetch');
 var redis = require('redis');
 var redClient = redis.createClient();
 var moment = require('moment');
+var argv = require('minimist')(process.argv.slice(2));
 const momFmt = 'YY-MM-DD hh:mm:ss';
-const apiKey = '5b9ba590615f3bc01e0b18c8cdd021a7';
 
 redClient.on('connect', function () {
   console.log(moment().format(momFmt) + ' Redis client connected');
@@ -26,9 +26,7 @@ redClient.on('error', function (err) {
   console.log(moment().format(momFmt) + ' Redis error:' + err);
 });
 
-//
 // Fetch EIA data
-//
 function goFetch() {
   function status(response) {
     if (response.status >= 200 && response.status < 300) {
@@ -41,7 +39,30 @@ function goFetch() {
     return response.json()
   }
 
-  const eiaUnit = 'MT.A';
+  // process commandline
+  let apiKey = argv.apikey; // filename from cmd line
+  let redisKey = argv.key; // redis-key from cmd line
+  let fuel = argv.fuel; // coal, oil or gas
+
+  if (apiKey === undefined || redisKey === undefined || fuel === undefined) {
+    console.log('Usage: node eia.js --fuel <coal|oil|gas> --apikey <apikey> ---key <rediskey>')
+    process.exit();
+  }
+
+  // create components of query
+  let eiaUnit = '';
+  let eiaSeriesName = '';
+  if (fuel === 'coal') {
+    eiaUnit = 'MT.A';
+    eiaSeriesName = 'INTL.7-1-';
+  } else if (fuel === 'oil') {
+    eiaUnit = 'TBPD.A';
+    eiaSeriesName = 'INTL.55-1-';
+  } else if (fuel === 'gas') {
+    eiaUnit = 'BCM.A';
+    eiaSeriesName = 'INTL.26-1-';
+  }
+
   const eiaRegions = [
     { region: 'Africa', code: 'AFRC' },
     { region: 'World', code: 'WORL' },
@@ -54,36 +75,49 @@ function goFetch() {
     { region: 'North America', code: 'NOAM' }
   ];
 
+  // build a single query URL, include all regions
   let url = 'https://api.eia.gov/series/?api_key=' + apiKey + '&series_id=';
-
   eiaRegions.forEach(element => {
-    url += 'INTL.7-1-' + element.code + '-' + eiaUnit + ';';
+    url += eiaSeriesName + element.code + '-' + eiaUnit + ';';
   });
 
   fetch(url)
     .then(status)
     .then(json)
     .then(results => {
-      if ((results.series.length === 0) || (results.series[0].data.length === 0)) {
+      // results.series is undefined if incorrect query URL
+      if ((results.series === undefined) || (results.series.length === 0) ||
+        (results.series[0].data === undefined) || (results.series[0].data.length === 0)) {
         console.log('No data from api.eia.gov', results);
-        return;
+        process.exit();
       }
 
+      // original EIA data array is:   [ [year,value], [], ...]
+      // convert to chart.js-friendly: [ {year,value}, {}, ...]
       results.series.forEach(series => {
         let d = series.data;
         series.data = d.map(x => ({
-          x: parseInt(x[0], 10),            // convert year to number
-          y: Math.round(x[1] * 100) / 100   // two decimals ok
+          x: parseInt(x[0], 10),
+          y: Math.round(x[1] * 100) / 100
         }));
       })
 
+      // add chart.js-friendly region-name to each series
+      // this avoids client having to do this
+      results.series.forEach(s => {
+        eiaRegions.forEach(r => {
+          if (s.series_id.indexOf(r.code) > -1) {
+            s.region = r.region;
+          }
+        });
+      });
+
       // Store key/value pair to Redis
-      let redisKey = 'eia-' + results.series_id;
       let redisValue = JSON.stringify(results);
       console.log(moment().format(momFmt) +
         ' Storing ' + redisValue.length +
         ' bytes, key=' + redisKey +
-        ' value=' + redisValue);//.substring(0, 60));
+        ' value=' + redisValue.substring(0, 60));
 
       redClient.set(redisKey, redisValue, function (error, result) {
         if (result) {
@@ -92,9 +126,11 @@ function goFetch() {
           console.log(moment().format(momFmt) + ' Error: ' + error);
         }
       });
+
+      // exit
       setInterval((() => {
         process.exit();
-      }), 2000)
+      }), 1000)
     })
     .catch(err => console.log(err))
 }
---
+++ connectors/eia/eia.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+# Wrapper for eia.js
+# Note that EIA API Key is in eiakey.txt
+# eiakey.txt is owned by root and not tracked by github
+# Create your own EIA API key: https://www.eia.gov/opendata/register.cfm
+#
+# H. Dahle 2020
+
+eiaapikeyfile='eiakey.txt'
+jsfile='eia.js'
+
+# Make sure eiakey exists
+if [ ! -f "${eiaapikeyfile}" ]; then
+  echo "EIA API key file not found: ${eiaapikeyfile} "
+  exit
+fi
+
+# read EIA KEY
+eiakey=`sudo cat ${eiaapikeyfile}`
+echo "Using EIA API key: ${eiakey}"
+
+# Make sure JS file exists
+if [ ! -f "${jsfile}" ]; then
+  echo "JS file not found: ${jsfile}"
+fi
+
+# Request Coal, Oil and Gas data
+for i in "coal" "oil" "gas" ; do
+  rediskey="eia-global-${i}"
+  node ${jsfile} --fuel ${i} --apikey ${eiakey} --key ${rediskey} 
+done
