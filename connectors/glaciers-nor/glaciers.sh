2b9f42b88c4475e56333b59ad51d9cfbff940fdd
Håkon Dahle <hakon.dahle@gmail.com>
2020-02-12 18:48:52 +0100

    working glacier CSV converter

--- connectors/glaciers-nor/glaciers.sh
+++ connectors/glaciers-nor/glaciers.sh
@@ -1,70 +1,82 @@
 #!/bin/sh
 
-# Fetch CO2 data from NOAA.GOV
+# Read glacier data from CSV
 # Convert to JSON and store to Redis
 #
 # H. Dahle
 
-REDISKEY="maunaloaco2-sm"
-TMPDIR=$(mktemp -d)
-CSVFILE="${TMPDIR}/${REDISKEY}.csv"
-JSONFILE="${TMPDIR}/${REDISKEY}.json"
-DATE=`date --iso-8601='minutes'`
+for FILE in *.csv ; do
+  
+  # It would be surprising if file not not found
+  if [ ! -f "$FILE" ]; then
+    echo "File not found $FILE"
+    exit
+  fi
 
-echo ${DATE}
-echo "Getting ftp data from noaa.gov, saving to ${CSVFILE}"
-curl ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_mm_mlo.txt > ${CSVFILE}
+  # Extract name of glacier from filename
+  GLACIER=`echo "$FILE" | awk -F_ '{print substr($NF,1,index($NF, ".csv")-1)}'`
+  echo $GLACIER
 
-if [ -f "${CSVFILE}" ]; then
-    echo -n "Number of lines in CSV:"
-    cat ${CSVFILE} | wc -l
-else
-    echo "File not found: ${CSVFILE}, aborting "
-    exit 0
-fi
 
-echo "Converting data to JSON, saving to ${JSONFILE}"
-awk -v d="${DATE}" 'BEGIN {ORS="";
+  REDISKEY="glacier-length-nor-${GLACIER}"
+  TMPDIR=$(mktemp -d)
+  CSVFILE="${FILE}"
+  JSONFILE="${TMPDIR}/${REDISKEY}.json"
+  DATE=`date --iso-8601='minutes'`
+
+
+  if [ -f "${CSVFILE}" ]; then
+      echo -n "Number of lines in CSV:"
+      cat ${CSVFILE} | wc -l
+  else
+      echo "File not found: ${CSVFILE}, aborting "
+      exit 0
+  fi
+
+  echo "Converting data to JSON, saving to ${JSONFILE}"
+  cat ${CSVFILE} | awk -v d="${DATE}" -v g="${GLACIER}" 'BEGIN {ORS=""; IRS="\r";  FS=";"
             print "{"
-            print "\"source\":"
-            print "\"Dr. Pieter Tans, NOAA/ESRL (www.esrl.noaa.gov/gmd/ccgg/trends/) and "
-            print "Dr. Ralph Keeling, Scripps Institution of Oceanography (scrippsco2.ucsd.edu/)\", "
-            print "\"link\":\"ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_mm_mlo.txt\", "
-            print "\"info\":"
-            print "\"Before the Industrial Revolution in the 19th century, global average CO2 was about 280 ppm. "
-            print "During the last 800,000 years, CO2 fluctuated between about 180 ppm during ice ages and "
-            print "280 ppm during interglacial warm periods.\", "
+            print "\"source\":\"NVE, The Norwegian Water Resources and Energy Directorate\", "
+            print "\"link\":\"http://glacier.nve.no/glacier/viewer/ci/no/\", "
             print "\"accessed\":\"" d "\", "
+            print "\"glacier\":\"" g "\", " 
             print "\"data\": ["
             NOTFIRST=0
            }
-     /^#/  {next}
-           {if (NOTFIRST) print ", "
+
+     /^#/        { next }
+     /Year/      { next }
+     $3=="Start" { $3=0 }
+
+           {
+            if (NOTFIRST) print ", "
             NOTFIRST=1
-            # printf "{\"date\":\"%s-%02d\",\"average\":%s,\"interpolated\":%s,\"trend\":%s}",  $1, $2, $4, $5, $6 }
-            printf "{\"x\":\"%s-%02d\",\"y\":%s}",  $1, $2, $5 }
-     END   {print "]}"}' < ${CSVFILE} > ${JSONFILE}
+            print "{\"x\":" $1 ",\"y\":" ($3+0) "}"
+           }
+     END   {print "]}"}' > ${JSONFILE}
 
-# Just for reassurance
-echo -n "JSON byte count:"
-cat ${JSONFILE} | wc --bytes
+  # Just for reassurance
+  echo -n "JSON byte count:"
+  cat ${JSONFILE} | wc --bytes
 
 
-# When installing cron job, provde fully qualified filename to this script
-REDIS=$1
-if [ "$REDIS" = "" ]; then
-  REDIS="redis-cli"
-else
-  if [ ! -f ${REDIS} ]; then
-    echo "Redis-executable not found: ${REDIS}, not storing in Redis"
-    exit
+  # When installing cron job, provde fully qualified filename to this script
+  REDIS=$1
+  if [ "$REDIS" = "" ]; then
+    REDIS="redis-cli"
+  else
+    if [ ! -f ${REDIS} ]; then
+      echo "Redis-executable not found: ${REDIS}, not storing in Redis"
+      exit
+    fi
   fi
-fi
+ 
+  # Save to redis
+  echo -n "Saving JSON to Redis, key: ${REDISKEY}: "
+  ${REDIS} -x set ${REDISKEY} < ${JSONFILE}
 
-# Save to redis
-echo -n "Saving JSON to Redis, key: ${REDISKEY}: "
-${REDIS} -x set ${REDISKEY} < ${JSONFILE}
+  # Quick verification
+  echo -n "Retrieving from Redis, JSON byte count: "
+  ${REDIS} get ${REDISKEY} | wc --bytes
 
-# Quick verification
-echo -n "Retrieving from Redis, JSON byte count: "
-${REDIS} get ${REDISKEY} | wc --bytes
+done
