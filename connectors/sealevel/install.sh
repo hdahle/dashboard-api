7220a5e1ce3442d103bb786e388ce455950a06e0
Håkon Dahle <hakon.dahle@gmail.com>
2020-02-18 15:02:29 +0100

    added csiro-alt-yearly.sh to install script and cleaned up a bit

--- connectors/sealevel/install.sh
+++ connectors/sealevel/install.sh
@@ -10,9 +10,12 @@ PWD=`pwd`
 
 # must use eval for tilde-expansion to work...dirty
 LOGDIR=`eval echo ~${USER}/log`
-SCRIPT="csiro-alt.sh"
 LOGFILE="${LOGDIR}/cron.log"
+TMPDIR=$(mktemp -d)
+NEWCRONTAB="${TMPDIR}/newcrontab.txt"
+
 REDIS=`which redis-cli`
+echo "Using tmp directory ${TMPDIR}"
 echo "Using Redis at ${REDIS}"
 echo "Logs are in ${LOGDIR}"
 
@@ -30,34 +33,33 @@ else
   echo "Using logfile: ${LOGFILE}"
 fi
 
-# make sure script exists
-if [ -f "${PWD}/${SCRIPT}" ]; then
-  echo "Shell script found"
-else
-  echo "Not found: ${PWD}/${SCRIPT} - aborting"
-  exit
-fi
+for SCRIPT in "csiro-alt.sh" "csiro-alt-yearly.sh" ; do
 
-# make new crontab entry, twice a month, on 11th and 21st
-NEWENTRY="0 0 11,21 * * ${PWD}/${SCRIPT} ${REDIS} >> ${LOGFILE} 2>&1"
-echo "Creating crontab entry: ${NEWENTRY}"
+  # make sure script exists
+  if [ -f "${PWD}/${SCRIPT}" ]; then
+    echo "Shell script found: ${PWD}/${SCRIPT}"
+  else
+    echo "Not found: ${PWD}/${SCRIPT} - aborting"
+    exit
+  fi
 
-# test if new entry already exists
-crontab -l > newcrontab
-EXISTENTRY=`grep -F "${NEWENTRY}" < newcrontab`
+  # make new crontab entry, once a month, on the 21st
+  NEWENTRY="0 0 21 * * ${PWD}/${SCRIPT} ${REDIS} >> ${LOGFILE} 2>&1"
+  echo "Creating crontab entry: ${NEWENTRY}"
 
-# add to crontab
-if [ "${EXISTENTRY}" = "${NEWENTRY}"  ]; then
-  echo "Already in crontab"
-else
-  echo "Adding new entry to crontab"
-  echo "${NEWENTRY}" >> newcrontab
-  crontab newcrontab
-fi
+  # test if new entry already exists
+  crontab -l > ${NEWCRONTAB}
+  EXISTENTRY=`grep -F "${NEWENTRY}" < ${NEWCRONTAB}`
 
-# clean up
-if [ -f newcrontab ]; then
-  echo "Cleaning up"
-  rm newcrontab
-fi
+  # add to crontab
+  if [ "${EXISTENTRY}" = "${NEWENTRY}" ]; then
+    echo "Already in crontab"
+  else
+    echo "Adding new entry to crontab"
+    echo "${NEWENTRY}" >> ${NEWCRONTAB}
+    crontab ${NEWCRONTAB}
+  fi
+
+done
+
 
