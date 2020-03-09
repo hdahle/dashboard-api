fc4d6c07c358616b6f208919d15f490780d096b7
Håkon Dahle <hakon.dahle@gmail.com>
2020-03-09 06:52:23 +0100

    fixed bug with population of Ireland

--- connectors/covid/covid-all.sh
+++ connectors/covid/covid-all.sh
@@ -311,6 +311,7 @@ gawk -v d="${DATE}" 'BEGIN {ORS=""
      $2=="Vatican City" { $2="Italy"}
      $2=="Saint Barthelemy" { $2="France"}
      $2=="Mainland China" { $2="China"}
+     $2=="Republic of Ireland" {$2=="Ireland"}
 
      # This line contains the list of dates, from $5 to $NF
      # Save the list of dates in the dates[] array
