983e44bf540c7705ab22c2c56ca8263f5590230f
Håkon Dahle <hakon.dahle@gmail.com>
2020-04-17 08:07:55 +0000

    updating .gitignores

---
+++ connectors/power-generation/update.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+
+node spain-electricity.js --year 2020 --key spain-electricity-2020
+
+exit
+
+sleep 30
+
+
+node spain-electricity.js --year 2019 --key spain-electricity-2019
+
+sleep 30
+
+node spain-electricity.js --year 2018 --key spain-electricity-2018
+
+sleep 30
+
+node spain-electricity.js --year 2017 --key spain-electricity-2017
+
+sleep 30
+
+node spain-electricity.js --year 2016 --key spain-electricity-2016
+
+sleep 30
+
+node spain-electricity.js --year 2015 --key spain-electricity-2015
+
+sleep 30
+
+node spain-electricity.js --year 2014 --key spain-electricity-2014
+
+sleep 30
+
+node spain-electricity.js --year 2013 --key spain-electricity-2013
+
+sleep 30
+
+node spain-electricity.js --year 2012 --key spain-electricity-2012
+
+sleep 30
+
+node spain-electricity.js --year 2011 --key spain-electricity-2011
+
+sleep 30
+
