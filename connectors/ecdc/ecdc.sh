c50d97ca746b1d4a9434638b66ad08fee71efd8a
Håkon Dahle <hakon.dahle@gmail.com>
2020-10-08 14:35:15 +0200

    shell script, tries three days, saves to redis

--- connectors/ecdc/ecdc.sh
+++ connectors/ecdc/ecdc.sh
@@ -1,9 +1,31 @@
 #!/bin/sh
+#
+# ECDC Weekly Corona Update
+#
+# H.Dahle, 2020
 
+TMPDIR=$(mktemp -d)
 SOURCEDIR="https://www.ecdc.europa.eu/sites/default/files/documents"
-FILENAME="subnational_weekly_data_2020-10-01.xlsx"
-OUTPUTFILE="ecdc.xlsx"
+TMPFILE="${TMPDIR}/ecdc.tmp"
+XLSXFILE="${TMPDIR}/ecdc.xlsx"
+REDISKEY="ecdc-weekly"
+FILENAME="subnational_weekly_data_"
 
-curl  "${SOURCEDIR}/${FILENAME}" --output ${OUTPUTFILE}
+DATETWODAYSAGO=`date -I -d "2 days ago"`
+DATEYESTERDAY=`date -I -d yesterday`
+DATETODAY=`date -I -d today`
 
+for DSTRING in $DATETWODAYSAGO $DATEYESTERDAY $DATETODAY
+do  
+  echo "Trying ${SOURCEDIR}/${FILENAME}${DSTRING}.xlsx"
+  echo "Saving to ${OUTPUTFILE}"
+  curl  "${SOURCEDIR}/${FILENAME}${DSTRING}.xlsx" --output ${TMPFILE}
 
+  if grep "DOCTYPE html" ${TMPFILE}  ; then 
+    echo No update from ECDC
+  else
+    mv ${TMPFILE} ${XLSXFILE}
+    echo "XLSX file stored in ${XLSXFILE}"
+    node ecdc.js --verbose --file ${XLSXFILE} --key ${REDISKEY}
+  fi
+done
