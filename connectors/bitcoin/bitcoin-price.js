15fbcc70e5572ea35babf9477e232d8fb0c10e62
Håkon Dahle <hakon.dahle@gmail.com>
2021-05-26 15:01:50 +0200

    improve exit. improve argv handling

--- connectors/bitcoin/bitcoin-price.js
+++ connectors/bitcoin/bitcoin-price.js
@@ -7,24 +7,41 @@
 // H. Dahle, 2021
 
 var fetch = require('node-fetch');
-var redis = require('redis');
-var redClient = redis.createClient();
+
 var moment = require('moment');
 var argv = require('minimist')(process.argv.slice(2));
 const momFmt = 'YY-MM-DD hh:mm:ss';
 
-redClient.on('connect', function () {
-  console.log(moment().format(momFmt) + ' Redis client connected');
-});
-redClient.on('ready', function () {
-  console.log(moment().format(momFmt) + ' Redis client ready');
-});
-redClient.on('warning', function () {
-  console.log(moment().format(momFmt) + ' Redis warning');
-});
-redClient.on('error', function (err) {
-  console.log(moment().format(momFmt) + ' Redis error:' + err);
-});
+function redisSave(redisKey, redisValue) {
+  let redis = require('redis');
+  let redClient = redis.createClient();
+  redClient.on('connect', function () {
+    console.log(moment().format(momFmt) + ' Redis client connected');
+  });
+  redClient.on('ready', function () {
+    console.log(moment().format(momFmt) + ' Redis client ready');
+  });
+  redClient.on('warning', function () {
+    console.log(moment().format(momFmt) + ' Redis warning');
+  });
+  redClient.on('error', function (err) {
+    console.log(moment().format(momFmt) + ' Redis error:' + err);
+  });
+  console.log(moment().format(momFmt) +
+    ' Bytes=' + redisValue.length +
+    ' Redis key=' + redisKey +
+    ' Value=' + redisValue.substring(0, 60));
+  redClient.set(redisKey, redisValue, function (error, result) {
+    if (result) {
+      console.log(moment().format(momFmt) + ' Result:' + result);
+    } else {
+      console.log(moment().format(momFmt) + ' Error: ' + error);
+    }
+    redClient.quit(function () {
+      console.log(moment().format(momFmt) + ' Redis quit');
+    });
+  });
+}
 
 // Fetch Bitcoin data
 (() => {
@@ -39,10 +56,11 @@ redClient.on('error', function (err) {
     return response.json()
   }
 
-  // process commandline
   const redisKey = argv.key;  // redis-key from cmd line
-
-  if (redisKey === undefined) {
+  const help = argv.help;
+  if (help || redisKey == true) {
+    console.log('Fetch daily Bitcoin price data from 5 years ago until today');
+    console.log('Uses data from api.coindesk.com');
     console.log('Usage: node bitcoin-price.js --key <rediskey>');
     process.exit();
   }
@@ -56,7 +74,6 @@ redClient.on('error', function (err) {
     .then(status)
     .then(json)
     .then(results => {
-      console.log(results)
       // results.series is undefined if incorrect query URL
       if (results.bpi === undefined) {
         console.log('No data from api.coindesk.com: ', results, url);
@@ -79,26 +96,13 @@ redClient.on('error', function (err) {
         }
       };
 
-      // Store key/value pair to Redis
+      // If redisKey defined store key/value pair to Redis. If not,just print to console
       const redisValue = JSON.stringify(json);
-      console.log(moment().format(momFmt) +
-        ' Bytes=' + redisValue.length +
-        ' Array length=' + json.data.length +
-        ' Redis key=' + redisKey +
-        ' Value=' + redisValue.substring(0, 60));
-
-      redClient.set(redisKey, redisValue, function (error, result) {
-        if (result) {
-          console.log(moment().format(momFmt) + ' Result:' + result);
-        } else {
-          console.log(moment().format(momFmt) + ' Error: ' + error);
-        }
-      });
-
-      // exit
-      setInterval((() => {
-        process.exit();
-      }), 1000)
+      if (redisKey) {
+        redisSave(redisKey, redisValue);
+      } else {
+        console.log(redisValue)
+      }
     })
     .catch(err => console.log(err))
 })();
