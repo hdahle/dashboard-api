45857549206ddd6697036db9dbd53698efc3a4eb
Håkon Dahle <hakon.dahle@gmail.com>
2021-10-12 10:19:41 +0200

    new connector: vaccines

---
+++ connectors/vaccines/vaccines.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# https://github.com/govex/COVID-19/blob/master/data_tables/vaccine_data/global_data/time_series_covid19_vaccine_global.csv
+curl "https://raw.githubusercontent.com/govex/COVID-19/master/data_tables/vaccine_data/global_data/time_series_covid19_vaccine_global.csv"
