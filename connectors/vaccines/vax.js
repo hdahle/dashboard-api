83502ead0724cce9c4d95b8121d729c7762498d3
Håkon Dahle <hakon.dahle@gmail.com>
2021-12-09 16:23:09 +0100

    vax.js simplified version of vaccines.js which is deprecated

--- connectors/vaccines/vax.js
+++ connectors/vaccines/vax.js
@@ -15,7 +15,7 @@ const regionNames = ['Oceania', 'Asia', 'Europe', 'Northern America', 'Latin Ame
 
 // Country population data from Wikipedia
 const worldPop = [
-  { c: "World", p: 7800000000, r: "World"},
+  { c: "World", p: 7800000000, r: "World" },
   { c: "China", p: 1433783686, r: "Asia" },
   { c: "India", p: 1366417754, r: "Asia" },
   { c: "US", p: 329064917, r: "Northern America" },
@@ -275,91 +275,85 @@ const worldPop = [
 
 // Parse commandline
 
-  const fn = argv.file; // filename from cmd line
-  const redisKey = argv.key; // redis-key from cmd line
-  const summaryOnly = argv.summaryOnly;
-  const verbose = argv.verbose;
-  let cList = (argv.countries === undefined) ? ['All'] : argv.countries.split(','); // list of countries
-  let countriesOK = cList.every(x =>
-    //['World', 'All', 'top20', 'regions'].includes(x) || worldPop.findIndex(c => c.c === x) > -1 || worldPop.findIndex(c => c.r === x) > -1
-    ['All'].includes(x) || worldPop.findIndex(c => c.c === x) > -1 || worldPop.findIndex(c => c.r === x) > -1
-    );
-  if (!countriesOK || fn === undefined || fn === true || !fs.existsSync(fn) || redisKey === undefined || redisKey === true || redisKey === '') {
-    console.log('Usage:\n\tnode vaccines.js --file <csvfile> [--countries <country,country,..,country>] --key <rediskey>');
-    console.log('\n')
-    //console.log('\t--countries=top20\n\t\tgenerate list of top 20 countries w.r.t. deaths per million')
-    //console.log('\t--countries=regions\n\t\tgenerate time-series data for Europe, Asia, Northern America, Latin America, Africa, Oceania')
-    console.log('\t--countries="Norway,France,Japan,South Korea"\n\t\tgenerate time-series data for the specified countries only')
-    console.log('\t--countries=All\n\t\tgenerate time-series data for the specified countries only')
-    process.exit();
-  }
-  if (cList[0] === 'All') {
-    cList = worldPop.map(x => x.c);
-  }
+const fn = argv.file; // filename from cmd line
+const redisKey = argv.key; // redis-key from cmd line
+const verbose = argv.verbose;
 
-  if (verbose) {
-    console.log(moment().format(momFmt) + ' Input file:', fn)
-    console.log(moment().format(momFmt) + ' Countries:', cList)
-    console.log(moment().format(momFmt) + ' Redis key:', redisKey)
-  }
-  if (cList[0] === 'regions') {
-    cList = regionNames;
-  }
+let partial = argv.partial;
+let full = argv.full;
+let doses = argv.doses;
 
-  // Create the Redisclient
-  // When the redis client is ready, start processing input file
-  var redClient = redis.createClient();
-  redClient.on('connect', function () {
-    console.log(moment().format(momFmt) + ' Redis client connected');
-  });
-  redClient.on('ready', function () {
-    console.log(moment().format(momFmt) + ' Redis client ready');
-    processFile(fn, redisKey, redClient, cList, summaryOnly);
-  });
-  redClient.on('warning', function () {
-    console.log(moment().format(momFmt) + ' Redis warning');
-  });
-  redClient.on('error', function (err) {
-    console.log(moment().format(momFmt) + ' Redis error:' + err);
-  });
-  redClient.on('end', function () {
-    console.log(moment().format(momFmt) + ' Redis closing');
-    process.exit();
-  });
+if (partial === undefined && full === undefined && doses === undefined) {
+  partial = true;
+  full = true;
+  doses = true;
+}
+
+let cList = (argv.countries === undefined) ? ['All'] : argv.countries.split(','); // list of countries
+let countriesOK = cList.every(x => ['All'].includes(x) || worldPop.findIndex(c => c.c === x) > -1);
+if (!countriesOK || fn === undefined || fn === true || !fs.existsSync(fn) || redisKey === true || redisKey === '') {
+  console.error('Usage:\n\tnode vaccines.js --file <csvfile> [--countries <country,country,...>] [--key <rediskey>] [--partial] [--full] [--doses]\n');
+  console.error('\t--countries="France,Japan,South Korea" Generate time-series data for the specified countries only');
+  console.error('\t    If no countries are specified, then all countries will be used\n');
+  console.error('\t--partial  Generate time-series data for partially vaccinated');
+  console.error('\t--full     Generate time-series data for fully vaccinated');
+  console.error('\t--doses    Generate time-series data for number of doses administered');
+  console.error('\t    If none of --partial,--full,--doses are specified, then data for all three will be generated');
+  process.exit();
+}
+if (cList[0] === 'All') {
+  cList = worldPop.map(x => x.c);
+}
+if (verbose) {
+  console.log(moment().format(momFmt) + ' Input file:', fn)
+  console.log(moment().format(momFmt) + ' Countries:', cList)
+  console.log(moment().format(momFmt) + ' Redis key:', redisKey)
+}
+
+processFile(fn, cList);
 
 //
 // Save to Redis, then quit Redis. On 'end' Redis will terminate this node.js process
 //
-function redisSave(key, value, redClient) {
-  console.log(moment().format(momFmt) + ' Store:' + value.length + ' Key=' + key + ' Val=' + value.substring(0, 60));
-  redClient.set(key, value, function (error, result) {
-    if (result) {
-      console.log(moment().format(momFmt) + ' Result:' + result);
-    } else {
-      console.log(moment().format(momFmt) + ' Error: ' + error);
-    }
-    redClient.quit(function () {
-      console.log(moment().format(momFmt) + ' Redis quit')
+function redisSave(key, value) {
+  let redClient = redis.createClient();
+  redClient.on('connect', function () {
+    if (verbose) console.log(moment().format(momFmt) + ' Redis client connected');
+  });
+  redClient.on('ready', function () {
+    if (verbose) console.log(moment().format(momFmt) + ' Redis client ready');
+    console.log(moment().format(momFmt) + ' Store:' + value.length + ' Key=' + key + ' Val=' + value.substring(0, 60));
+    redClient.set(key, value, function (error, result) {
+      if (result) {
+        console.log(moment().format(momFmt) + ' Result:' + result);
+      } else {
+        console.error(moment().format(momFmt) + ' Error: ' + error);
+      }
+      redClient.quit(function () {
+        console.log(moment().format(momFmt) + ' Redis quit')
+      });
     });
+  });
+  redClient.on('warning', function () {
+    if (verbose) console.log(moment().format(momFmt) + ' Redis warning');
+  });
+  redClient.on('error', function (err) {
+    if (verbose) console.log(moment().format(momFmt) + ' Redis error:' + err);
+  });
+  redClient.on('end', function () {
+    if (verbose) console.log(moment().format(momFmt) + ' Redis closing');
+    process.exit();
   });
 }
 
 //
 // Process input-file line-by line
+// CSV input columns:
+// Country_Region,Date,Doses_admin,People_partially_vaccinated,People_fully_vaccinated,Report_Date_String,UID,Province_State
 //
-function processFile(fn, redisKey, redClient, cList, summaryOnly) {
+function processFile(fn, cList) {
   let result = [];
   let firstLineFound = false;
-  // result = [{
-  //      country,
-  //      region,
-  //      data {
-  //        datasets[{
-  //          label = partial | full,
-  //          data[{date,value}]
-  //        }
-  //      }]
-  //    }]
 
   fs.createReadStream(fn)
     .pipe(parse({ delimiter: ',' }))
@@ -369,77 +363,76 @@ function processFile(fn, redisKey, redClient, cList, summaryOnly) {
         firstLineFound = true;
         return;
       }
-
       // Skip this, we already have US
       if (csv[0] === 'US (Aggregate)') {
         return;
       }
-      
       // Skip regions, they are included in country data
-      if (csv[7].length>0) {
+      if (csv[7].length > 0) {
         return;
       }
       // Now process each line of country-data
       let cName = countryName(csv[0]);
       let country = worldPop.find(x => x.c === cName);
       if (country === undefined) {
-        console.log('Warning: Undefined country, skipping', cName);
+        console.error('Warning: Undefined country, skipping', cName);
         return;
       }
-
       // Only process countries specified in cList
       if (!cList.find(x => x === country.c)) {
         return;
       }
-
       // Does country already exist in result? If not, push new country     
       let a = result.find(x => x.country == country.c);
       if (a === undefined) {
-        if (verbose) console.log('Processing', country.c);
         a = {
           country: country.c,
           region: country.r,
-          data:{
-            datasets: [{
-              label: "Partial",
-              data: []
-            },{
-              label: "Full",
-              data: []
-            }]
+          data: {
+            labels: [],
+            datasets: []
           }
         }
+        if (partial) a.data.datasets.push({ label: "Partial", data: [] })
+        if (full) a.data.datasets.push({ label: "Full", data: [] })
+        if (doses) a.data.datasets.push({ label: "Doses", data: [] })
         result.push(a)
       }
-      a.data.datasets[0].data.push({
-        x: csv[1], y: Math.trunc(1000 *csv[3] / country.p)/10, n: parseInt(csv[3],10)
-      })
-      a.data.datasets[1].data.push({  
-        x: csv[1], y: Math.trunc(1000 *csv[4] / country.p)/10, n: parseInt(csv[4],10)
-      })
+      // Dates go into the labels array
+      a.data.labels.push(csv[1]);
+      // Partially vaccinated
+      if (partial) a.data.datasets[0].data.push(Math.trunc(1000 * csv[3] / country.p) / 10);
+      // Fully vaccinated
+      if (full) a.data.datasets[1].data.push(Math.trunc(1000 * csv[4] / country.p) / 10);
+      // Number of doses
+      if (doses) a.data.datasets[2].data.push(parseInt(csv[2], 10));
     })
     .on('end', () => {
-      console.log('Number of countries:', result.length)
+      if (verbose) console.log('Number of countries:', result.length)
       if (!firstLineFound) {
-        console.log('Error: First line in file not found');
+        console.error('Error: First line in file not found');
         return;
       }
-      // Store key/value pair to Redis
       let val = {
         source: 'Johns Hopkins University, https://github.com/govex/COVID-19',
         link: 'https://github.com/govex/COVID-19/blob/master/data_tables/vaccine_data/global_data/time_series_covid19_vaccine_global.csv',
-        info: 'Data is specified thus: { x: date, y: percentage-of-population-vaccinated, n: number-of-people-vaccinated}',
+        info: 'Three datasets per country: [0] Partially vaccinated, [1] Fully vaccinated, [2] Total number of doses administered',
         accessed: moment().format('YYYY-MM-DD hh:mm'),
         numberOfCountries: result.length,
         data: result
       };
-      redisSave(redisKey, JSON.stringify(val), redClient);
+      // Store resulting JSON to redis or print to stdout
+      if (redisKey === undefined) {
+        console.log(JSON.stringify(val));
+      } else {
+        redisSave(redisKey, JSON.stringify(val));
+      }
     })
     .on('close', () => {
       console.log('Readstream closed');
     })
     .on('error', err => {
-      console.log('Readstream error:', err)
+      console.error('Readstream error:', err)
     })
 }
 
