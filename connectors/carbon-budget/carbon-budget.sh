a8bfdaebf573a9ab0c64b0760a577a680f47ca53
Håkon Dahle <hakon.dahle@gmail.com>
2022-11-16 17:02:05 +0100

    2022 update Global Carbon Project

--- connectors/carbon-budget/carbon-budget.sh
+++ connectors/carbon-budget/carbon-budget.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-node fossil-emissions-by-fuel-type-2020.js --file Global_Carbon_Budget_2020v1.0_Emissions-by-type.csv --key emissions-by-fuel-type
+# 2022
 
-node national-carbon-emissions-2020.js --file National_Carbon_Emissions_2020v1.0_Territorial.csv --key "emissions-by-region" --countries "World,Africa,Asia,Central America,Europe,Middle East,North America,South America,Oceania,Bunkers"
+cite2022="Global Carbon Project. (2022). Supplemental data of Global Carbon Budget 2022 (Version 1.0) [Data set]. Global Carbon Project. https://doi.org/10.18160/gcp-2022"
+link2022="https://www.icos-cp.eu/science-and-impact/global-carbon-budget/2022"
+node fossil-emissions-by-fuel-type-2021.js --file Global_Carbon_Budget_2022v1.0_Emissions-by-type.csv --key emissions-by-fuel-type-2022 --source=$cite2022 --link=$link2022
+node national-carbon-emissions-2021.js --file National_Carbon_Emissions_2022v1.0_Territorial.csv --key emissions-by-region-2022 --countries "World,Africa,Asia,Central America,Europe,Middle East,North America,South America,Oceania,Bunkers"  --source=$cite2022 --link=$link2022
+
+# 2021
+
+node fossil-emissions-by-fuel-type-2021.js --file Global_Carbon_Budget_2021v1.0_Emissions-by-type.csv --key emissions-by-fuel-type
+node national-carbon-emissions-2021.js --file National_Carbon_Emissions_2021v1.0_Territorial.csv --key "emissions-by-region" --countries "World,Africa,Asia,Central America,Europe,Middle East,North America,South America,Oceania,Bunkers"
